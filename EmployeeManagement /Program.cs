97c8442273b97f4eaf688017b1c918021ed4beba
luis <lrodriguez@soluum.com>
2025-01-01 23:48:08 -0800

    Add logging.

--- EmployeeManagement /Program.cs
+++ EmployeeManagement /Program.cs
@@ -1,13 +1,17 @@
+using EmployeeManagement.Tools;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IWebAppLogger, WebAppLogger>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    Console.WriteLine("development environment");
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
---
+++ EmployeeManagement /Tools/WebAppLogger.cs
@@ -0,0 +1,38 @@
+using System.Runtime.CompilerServices;
+
+namespace EmployeeManagement.Tools;
+
+public interface IWebAppLogger
+{
+    void LogInfo(string message, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string path = null,
+        [CallerMemberName] string caller = null);
+
+    void LogWarning(string message, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string path = null,
+        [CallerMemberName] string caller = null);
+
+    void LogError(string message, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string path = null,
+        [CallerMemberName] string caller = null);
+}
+public class WebAppLogger : IWebAppLogger
+{
+    public void LogInfo(string message, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string path = null,
+        [CallerMemberName] string caller = null)
+    {
+        Console.WriteLine(
+            $"{DateTime.UtcNow.ToString("O")} | Info | TID: {Thread.CurrentThread.ManagedThreadId} | {Path.GetFileNameWithoutExtension(path)} | {caller}() | Line: {lineNumber} | {message}");
+    }
+
+    public void LogWarning(string message, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string path = null,
+        [CallerMemberName] string caller = null)
+    {
+        Console.WriteLine(
+            $"{DateTime.UtcNow.ToString("O")} | Warning | TID: {Thread.CurrentThread.ManagedThreadId} | {Path.GetFileNameWithoutExtension(path)} | {caller}() | Line: {lineNumber} | {message}");
+    }
+
+    public void LogError(string message, [CallerLineNumber] int lineNumber = 0, [CallerFilePath] string path = null,
+        [CallerMemberName] string caller = null)
+    {
+        Console.WriteLine(
+            $"{DateTime.UtcNow.ToString("O")} | Error | TID: {Thread.CurrentThread.ManagedThreadId} | {Path.GetFileNameWithoutExtension(path)} | {caller}() | Line: {lineNumber} | {message}");
+    }
+}
